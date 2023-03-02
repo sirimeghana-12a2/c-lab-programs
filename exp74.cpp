94047efee148c41b63d78cce02ecc2ae311503c7
sirimeghana-12a2 <111947363+sirimeghana-12a2@users.noreply.github.com>
2023-03-02 12:08:17 +0530

    Add files via upload

---
+++ exp74.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class A
+{
+	protected:
+		int x;
+	public:
+		A() {x=10;}
+};
+class B
+{
+	protected:
+		int x;
+	public:
+		B() {x=5;}
+};
+class C:public A,public B
+{
+	public:
+		void fun()
+		{
+			cout<<"A's x is:"<<A::x<<endl;
+			cout<<"B's x is:"<<B::x<<endl;
+		}
+};
+int main()
+{
+	C c;
+	c.fun();
+	return 0;
+}
