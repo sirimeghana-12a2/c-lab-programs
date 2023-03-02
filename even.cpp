7f6be9fec3dbdd511af7b07c48ec7dc1348b1e63
sirimeghana-12a2 <111947363+sirimeghana-12a2@users.noreply.github.com>
2023-03-02 12:05:21 +0530

    Add files via upload

---
+++ even.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter number:"<<endl;
+	cin>>n;
+	if(n%2==0)
+	{
+	   cout<<"Number is even"<<endl;
+	}
+	else
+	{
+	   cout<<"Number is odd"<<endl;
+	}
+}
+	
