02f3b331db42de580cb77bb3cc2e55c6db66ba06
sirimeghana-12a2 <111947363+sirimeghana-12a2@users.noreply.github.com>
2023-03-02 12:03:30 +0530

    Add files via upload

---
+++ arthmetic.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"Enter a and b:"<< endl;
+	cin>>a>>b;
+	c=a+b;
+	cout<<"sum="<<c<<endl;
+	c=a-b;
+	cout<<"sub="<<c<<endl;
+	c=a*b;
+	cout<<"multiplication="<<c<<endl;
+	c=a/b;
+	cout<<"division="<<c<<endl;
+	c=a%b;
+	cout<<"remainder="<<c<<endl;
+	return 0;
+}
